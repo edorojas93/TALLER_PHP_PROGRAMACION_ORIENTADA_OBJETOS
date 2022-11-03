3f7ce9e4b1ee9fa049a835dca2bf36583371d11e
edorojas93 <edo.rojas.10@gmail.com>
2022-11-03 12:07:05 -0500

    Ejercicio 02

--- api.php
+++ api.php
@@ -1,82 +1,35 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json; charset=utf-8');
-    $_DATA = json_decode(file_get_contents("php://input"));
-    class zodiacal{
-        public $dia;
-        public $mes;
-        public $lista;
-        public function __construct(int $dia, int $mes){
-            $this->dia = $dia;
-            $this->mes = $mes;
-            $this->lista = (object) [
-                "Aries" =>  [
-                    "0" => [18, 4],
-                    "1" => [13, 5],
-                ],
-                "Tauro" =>  [
-                    "0" => [14, 5],
-                    "1" => [19, 6],
-                ],
-                "Gémenis" =>  [
-                    "0" => [20, 6],
-                    "1" => [20, 7],
-                ],
-                "Cáncer" =>  [
-                    "0" => [21, 7],
-                    "1" => [9, 8],
-                ],
-                "Leo" =>  [
-                    "0" => [10, 8],
-                    "1" => [15, 9],
-                ],
-                "Virgo" =>  [
-                    "0" => [16, 9],
-                    "1" => [30, 10],
-                ],
-                "Libra" => [
-                    "0" => [31, 10],
-                    "1" => [22, 11],
-                ],
-                "Escorpio" =>    [
-                    "0" => [23, 11],
-                    "1" => [29, 11],
-                ],
-                "Ofiuco" =>    [
-                    "0" => [30, 11],
-                    "1" => [17, 12],
-                ],
-                "Sagitario" =>    [
-                    "0" => [18, 12],
-                    "1" => [18, 1],
-                ],
-                "Capricornio" =>    [
-                    "0" => [19, 1],
-                    "1" => [15, 2],
-                ],
-                "Acuario" =>    [
-                    "0" => [16, 2],
-                    "1" => [11, 3],
-                ],
-                "Piscis" =>    [
-                    "0" => [12, 3],
-                    "1" => [17, 4],
-                ]
-            ];
+    $_DATA = json_decode(file_get_contents("php://input"),true);
+
+    class galeria{
+        public $codigo;
+        public $cantidad;
+
+        public function __construct(int $codigo, int $cantidad){
+            $this->codigo = $codigo;
+            $this->cantidad = $cantidad;
         }
-        public function buscar():string{
-            $res = "";
-            foreach ($this->lista as $key => $value) {
-                //Maldita inclusion de m*****
-                if(($this->dia == 28 && 3 == $this->mes) || ($this->dia == 29 && 3 == $this->mes)){
-                    $res = "Cetus";
-                }else if(($this->dia >= $value[0][0] && $value[0][1] == $this->mes) || ($this->dia <= $value[1][0] && $value[1][1] == $this->mes)){
-                    $res = $key;
-                }
-            }
-            return $res;
+
+        public function codigos(){
+            $codigo = match($this->codigo){
+                1 => $codigo = 200,
+                2 => $codigo = 100,
+                3 => $codigo = 80,
+                
+            };
+            return $codigo;
+        }
+
+        public function pagar():string{
+            $codigo = $this->codigos();
+            $pagar = $codigo*$this->cantidad;
+            return $pagar;
         }
     }
-    $obj = new zodiacal($_DATA->dia, $_DATA->mes);
-    echo json_encode(["mensaje" => $obj->buscar()],JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+    $lista = new galeria(codigo:$_DATA['codigo'],cantidad:$_DATA['cantidad']);
+    echo($lista->pagar());
+
 ?>
--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-addEventListener("DOMContentLoaded", (e)=>{
-    // 1.Este programa muestra el signo zodiacal de una persona. 
-    // Para ello el usuario debe introducir únicamente el día y 
-    // el mes de nacimiento y el programa determinará inmediatamente 
-    // el signo zodiacal de la persona.
-    let myForm = document.querySelector("#Formulario");
-    myForm.addEventListener("submit", async(e)=>{
+addEventListener("DOMContentLoaded",(e)=>{
+    let myform=document.querySelector("#form");
+    myform.addEventListener("submit",async(e)=>{
         e.preventDefault();
-        let data = Object.fromEntries(new FormData(e.target));
+        let form = e.target;
+        let data = Object.fromEntries(new FormData(form));
+        document.querySelector("#res").innerHTML = "";
+
         let config = {
-            method: myForm.method,
+            method: form.method,
             body: JSON.stringify(data)
         };
-        let peticion = await fetch(myForm.action,config);
-        let res = await peticion.json();
-        document.querySelector("#res").innerHTML = res.mensaje;
-        
+
+        let peticion = await fetch(form.action,config);
+        let respuesta = await peticion.text();
+
+        document.querySelector("#res").insertAdjacentHTML("beforeend",respuesta);
     })
+
+})
