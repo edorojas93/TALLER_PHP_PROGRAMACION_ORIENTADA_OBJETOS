d242b33c945d3a61d253bf20f047c07e7cedba18
edorojas93 <edo.rojas.10@gmail.com>
2022-11-03 12:35:47 -0500

    Ejercicio 04

--- api.php
+++ api.php
@@ -3,59 +3,9 @@
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
 
-    class promedio{
-        public $sumaA;
-        public $cantA;
-        public $sumaB;
-        public $cantB;
-        public $sumaC;
-        public $cantC;
-
-        public function __construct(int $sumaA,int $canA,int $sumaB,int $canB, int $sumaC,int $canC){
-            $this->sumaA = $sumaA;
-            $this->canA = $cantA;
-            $this->sumaB = $sumaB;
-            $this->canB = $cantB;
-            $this->sumaC = $sumaC;
-            $this->canC = $cantC;
-        }
-
-        public function promedioF():string{
-
-            $sumaA = $this->sumaA;
-            $cantA = $this->canA;
-            $sumaB = $this->sumaB;
-            $cantB = $this->canB;
-            $sumaC = $this->sumaC;
-            $cantC = $this->canC;
-
-            $cantA<1?$promA=0:$promA = $sumaA/$cantA;
-            $cantB<1?$promB=0:$promB = $sumaB/$cantB;
-            $cantC<1?$promC=0:$promC = $sumaC/$cantC;
-
-            if($promA>$promB && $promA>$promC){
-                if($promB>$promC){
-                    return "C";
-                } else{
-                    return "B";
-                }
-            } else if($promB>$promA && $promB>$promC){
-                if($promA>$promC){
-                    return "C";
-                } else{
-                    return "A";
-                }
-            } else if($promC>$promA && $promC>$promB){
-                if($promA>$promB){
-                    return "B";
-                } else{
-                    return "A";
-                }
-            } else{
-                return "A, B, C";
-            }
+    class pagos{
+        public function __construct(){
         }
     }
-    $lista = new promedio(sumaA:$_DATA[0],cantA:$_DATA[1],sumaB:$_DATA[2],cantB:$_DATA[3],sumaC:$_DATA[4],cantC:$_DATA[5]);
-    echo($lista->promedioF());
+    $lista = new pagos();
 ?>
--- main.js
+++ main.js
@@ -1,85 +1,25 @@
-let sumaA=0
-let sumaB=0
-let sumaC=0
-let cantA=0
-let cantB=0
-let cantC=0;
-
-function sumaPro(genero, carrera, edad) {
-    if(genero=="Masculino"){
-        if(carrera=="a"){
-            sumaA= sumaA+edad;
-            cantA +=1;
-        }else if (carrera=="b"){
-            sumaB= sumaB+edad;
-            cantB +=1;
-        }else{
-            sumaC= sumaC+edad;
-            cantC +=1;
-        }
-    } else{
-        null
-    }
-}
-
 addEventListener("DOMContentLoaded", (e) => {
-    document.querySelector("#cantidad").removeAttribute("disabled");
-    document.querySelector("#cantidad").addEventListener("keyup", (e) => {
-        let genero = document.querySelector("#genero");
-        let edad = document.querySelector("#edad");
-        let carrera = document.querySelector("#carrera");
-        if (e.target.value > 0) {
-            genero.removeAttribute("disabled");
-            edad.removeAttribute("disabled");
-            carrera.removeAttribute("disabled");
-        } else {
-            genero.disabled = true;
-            edad.disabled = true;
-            carrera.disabled = true;
-        }
-    })
-    let cont = 1;
-    let lista = [];
-    let myform = document.querySelector("#form");
-    myform.addEventListener("submit", async (e) => {
+    let form = document.querySelector("#form");
+    form.addEventListener("submit", async (e) => {
         e.preventDefault();
-        let cantidad = document.querySelector("#cantidad").value;
-        document.querySelector("#cantidad").disabled = true;
-        if (cont < cantidad) {
-            let data = Object.fromEntries(new FormData(form));
-            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
-            document.querySelector("#genero").selectedIndex = 0;
-            document.querySelector("#edad").value = "";
-            document.querySelector("#carrera").selectedIndex = 0;
-            cont += 1;
-
-        } else {
-            let form = e.target;
-            let data = Object.fromEntries(new FormData(form));
-            sumaPro(data["genero"], data["carrera"], parseInt(data["edad"]));
-            document.querySelector("#res").innerHTML = "";
-            lista.push(sumaA);
-            lista.push(cantA);
-            lista.push(sumaB);
-            lista.push(cantB);
-            lista.push(sumaC);
-            lista.push(cantC);
-            
-            let config = {
-                method: form.method,
-                body: JSON.stringify(lista)
-            };
-
-            let peticion = await fetch(form.action, config);
-            let respuesta = await peticion.text();
-            document.querySelector("#res").insertAdjacentHTML("beforeend", respuesta);
-            document.querySelector("#genero").selectedIndex = 0;
-            document.querySelector("#edad").value = "";
-            document.querySelector("#carrera").selectedIndex = 0;
-            genero.disabled = true;
-            edad.disabled = true;
-            carrera.disabled = true;
+        let meses = document.querySelector("#meses").value;
+        let dato = new Date(meses);
+        let ano
+        let mes
+        let dia;
+        var pago = 10;
+        if (!!dato.valueOf()) {
+            ano = dato.getFullYear();
+            mes = dato.getMonth() + 1;
+            dia = dato.getDate() + 1;
         }
-
+        let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+        let nombresMes = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        for(let i=0;i<20;i++){
+            var indice = new Date(ano, mes - 1, 16).getDay();
+            document.querySelector("#res").insertAdjacentHTML("beforeend",(`${diasSemana[indice]} del ${dia} de ${nombresMes[new Date(ano, mes - 1, dia).getMonth()]} del ${(new Date(year, mes - 1, dia).getMonth()==0) ?++ano : ano} debe pagar ${pagos}<br>`));
+            pagos=pagos*2;
+            mes++;
+        }
     })
 })
